5712aa541e0b12aeab992ca5d5757fccfff8e023
manjimnav1 <manjimnav1@9c7d887f-236e-49a9-a068-b85db8d636d3>
2017-12-21 16:17:43 +0000

    Arreglado populate y repositorios porque a juanmi no le iba xdXDdddddejjejejjnaj

--- Acme-Explorer/src/main/java/repositories/TagRepository.java
+++ Acme-Explorer/src/main/java/repositories/TagRepository.java
@@ -12,6 +12,6 @@ import domain.Tag;
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Integer> {
 
-	@Query("select ta from Trip t join t.tags ta where t.id = ?1")
+	@Query("select tv.tag from Trip t join t.tagValues tv where t.id = ?1")
 	Collection<Tag> findTagsByTrip(int tripId);
 }
